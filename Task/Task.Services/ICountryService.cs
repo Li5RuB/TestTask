0868415acf402c66433811c1791c6c80e37d603d
ITRANSITION\z.koshin <z.koshin@itransition.com>
2022-01-11 15:05:41 +0300

    created services

---
+++ Task/Task.Services/ICountryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Repository.Entities;
+
+namespace Task.Services
+{
+    public interface ICountryService
+    {
+        public Task<IEnumerable<Country>> GetAll();
+
+        public Task<Country> GetById(int id);
+
+        public Task<IEnumerable<City>> GetCities();
+    }
+}
---
+++ Task/Task.Services/IUserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Repository.Entities;
+
+namespace Task.Services
+{
+    public interface IUserService
+    {
+        public Task<IEnumerable<User>> GetUsers(Expression<Func<User, bool>> expression);
+
+        public Task<User> GetById(int id);
+
+        public Task<IEnumerable<User>> GetAll();
+
+        public void CreateUser(User user);
+
+        public void RemoveUser(User user);
+
+        public void UpdateUser(User user);
+
+        public Task<IEnumerable<User>> GetByPage(int page);
+
+        public Task<IEnumerable<User>> Search(string search);
+
+        public void SaveChanges();
+    }
+}
