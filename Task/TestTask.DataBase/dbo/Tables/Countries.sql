4213b1173cd88701ae44db518c38596606eb1b82
ITRANSITION\z.koshin <z.koshin@itransition.com>
2022-02-04 15:53:17 +0300

    CreateProject

---
+++ Task/TestTask.DataBase/dbo/Tables/Countries.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Countries] (
+    [Name]        VARCHAR (255) NOT NULL,
+    [CountryId]   INT           IDENTITY (1, 1) NOT NULL,
+    [Description] VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([CountryId] ASC)
+);
+
