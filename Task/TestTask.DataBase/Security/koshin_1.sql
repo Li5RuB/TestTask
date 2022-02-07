a3c7dac7a899081bb362d9d01e4cda66d774066a
ITRANSITION\z.koshin <z.koshin@itransition.com>
2022-02-07 10:57:47 +0300

    fixe

--- Task/TestTask.DataBase/Security/koshin_1.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [koshin]
-    WITH PASSWORD = N'of|pnzyW{l{yjufxmverbessmsFT7_&#$!~<poz{Y4@Mjt1Y', SID = 0x902D88B6B7C2F840BF23CE20D61BF060, DEFAULT_DATABASE = [TestTask], DEFAULT_LANGUAGE = [us_english];
-
