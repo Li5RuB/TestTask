6538345c9add114e08d765d743d709e42a026090
ITRANSITION\z.koshin <z.koshin@itransition.com>
2022-03-04 11:04:33 +0300

    ref stored procedure

--- Task/TestTask.DataBase/dbo/Stored Procedures/LastLoginStatistics.sql
+++ Task/TestTask.DataBase/dbo/Stored Procedures/LastLoginStatistics.sql
@@ -1,6 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[LastLoginStatistics]
 AS
-    SELECT c.Name AS 'CountryName', Count(u.UserId) AS 'UsersCount', Max(u.LastLogin) AS 'LastLoginFromCountry'
-    FROM Users u inner join Cities s on u.CityId = s.CityId inner join Countries c on s.CountryId = c.CountryId
-    Group by c.Name
+    SELECT c.Name AS 'CountryName', COUNT(u.UserId) AS 'UsersCount', MAX(u.LastLogin) AS 'LastLoginFromCountry'
+    FROM Users u 
+    INNER JOIN Cities s ON u.CityId = s.CityId 
+    INNER JOIN Countries c ON s.CountryId = c.CountryId
+    GROUP BY c.Name
 RETURN 0
